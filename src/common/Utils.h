16c124a052c03dddb53cdb050b3f3f2fc31be4ef
Michał Walenciak <michalwalenciak@gmail.com>
2026-05-02 21:59:46 +0200

    Add missing include

--- src/common/Utils.h
+++ src/common/Utils.h
@@ -1,8 +1,10 @@
 
 #pragma once
 
+#include <cstdint>
 #include <span>
 #include <vector>
+
 
 std::string formatBytes(std::uint64_t bytes);
 std::string formatTable(const std::span<std::vector<std::string>> rows);
